df8a27fbed8eaa6e46e3716e48949f25b565b5ba
nhatbass <nguyenbanhat090407@gmail.com>
2026-05-02 21:03:09 +0700

    chạy trên cả 3 hệ điều hành

--- src/test/java/example/CalculatorTest.java
+++ src/test/java/example/CalculatorTest.java
@@ -4,6 +4,8 @@ import com.example.Calculator;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -14,13 +16,22 @@ public class CalculatorTest {
         // Kiểm tra 5 + 10 = 15
         assertEquals(99, calc.add(5, 10));
     }
-    @Test
-    public void testFilePathOS() {
-        // Cố tình gõ đường dẫn cứng theo kiểu Windows (dùng dấu \)
-        File file = new File("config\\settings.xml");
+//    @Test
+//    public void testFilePathOS() {
+//        // Cố tình gõ đường dẫn cứng theo kiểu Windows (dùng dấu \)
+//        File file = new File("config\\settings.xml");
+//
+//        // Trên Windows: Máy tính tự hiểu "config" là thư mục mẹ. getParent() trả về "config".
+//        // Trên Linux/Mac: Máy tính hiểu "config\settings.xml" là... tên của 1 cái file dài ngoẵng. getParent() trả về null.
+//        assertEquals("config", file.getParent());
+//    }
 
-        // Trên Windows: Máy tính tự hiểu "config" là thư mục mẹ. getParent() trả về "config".
-        // Trên Linux/Mac: Máy tính hiểu "config\settings.xml" là... tên của 1 cái file dài ngoẵng. getParent() trả về null.
-        assertEquals("config", file.getParent());
+    @Test
+    public void testFilePathOSFixed() {
+        // Dùng API Path của Java: Nó sẽ tự động nối chuỗi bằng "\" nếu chạy trên Win, và "/" nếu chạy trên Linux
+        Path path = Paths.get("config", "settings.xml");
+
+        // Bây giờ thì chạy ở đâu thư mục mẹ cũng sẽ tên là "config"
+        assertEquals("config", path.getParent().toString());
     }
 }
