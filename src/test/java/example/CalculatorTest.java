0393d7aa2a289b2dc3e1249e233dc816aaf6d9a9
nhatbass <nguyenbanhat090407@gmail.com>
2026-05-02 20:58:38 +0700

    chỉ chạy trên window

---
+++ src/test/java/example/CalculatorTest.java
@@ -0,0 +1,26 @@
+package java.example;
+
+import com.example.Calculator;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+    @Test
+    public void testAdd() {
+        Calculator calc = new Calculator();
+        // Kiểm tra 5 + 10 = 15
+        assertEquals(99, calc.add(5, 10));
+    }
+    @Test
+    public void testFilePathOS() {
+        // Cố tình gõ đường dẫn cứng theo kiểu Windows (dùng dấu \)
+        File file = new File("config\\settings.xml");
+
+        // Trên Windows: Máy tính tự hiểu "config" là thư mục mẹ. getParent() trả về "config".
+        // Trên Linux/Mac: Máy tính hiểu "config\settings.xml" là... tên của 1 cái file dài ngoẵng. getParent() trả về null.
+        assertEquals("config", file.getParent());
+    }
+}
